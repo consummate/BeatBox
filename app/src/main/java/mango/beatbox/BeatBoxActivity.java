a6b042a0e144326dd272ae467e96192887bdced1
Irene zhao <irene_syz@163.com>
2016-12-16 22:13:44 +0800

    Initial commit

---
+++ app/src/main/java/mango/beatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package mango.beatbox;
+
+
+import android.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
